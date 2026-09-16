615053882e48154825370d0cd0d4521dc4b81ba1
Petro <petropavliuk12.05@gmail.com>
2026-09-16 19:29:28 +0300

    added test viewset templates

--- airport/views.py
+++ airport/views.py
@@ -1,7 +1,25 @@
 from rest_framework import viewsets
 
-from airport.models import AirplaneType
-from airport.serializers import AirplaneTypeSerializer
+from airport.models import (
+    AirplaneType,
+    Airport,
+    Route,
+    Crew,
+    Ticket,
+    Airplane,
+    Flight,
+    Order,
+)
+from airport.serializers import (
+    AirplaneTypeSerializer,
+    AirportSerializer,
+    RouteSerializer,
+    CrewSerializer,
+    TicketSerializer,
+    AirplaneSerializer,
+    FlightSerializer,
+    OrderSerializer,
+)
 
 
 class AirplaneTypeViewSet(
@@ -9,3 +27,52 @@ class AirplaneTypeViewSet(
 ):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+
+
+class AirportViewSet(
+    viewsets.ModelViewSet,
+):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class RouteViewSet(
+    viewsets.ModelViewSet,
+):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class CrewViewSet(
+    viewsets.ModelViewSet,
+):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class TicketViewSet(
+    viewsets.ModelViewSet,
+):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class AirplaneViewSet(
+    viewsets.ModelViewSet,
+):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+
+class OrderViewSet(
+    viewsets.ModelViewSet,
+):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class FlightViewSet(
+    viewsets.ModelViewSet,
+):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
