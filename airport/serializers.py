befebbdb64a4be85f07a76a7268a18f3c1a6f501
Petro <petropavliuk12.05@gmail.com>
2026-09-17 14:40:42 +0300

    added OrderDetailSerializer

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,4 +1,6 @@
+from django.db import transaction
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from airport.models import (
     AirplaneType,
@@ -87,19 +89,42 @@ class CrewSerializer(serializers.ModelSerializer):
         ]
 
 
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = "__all__"
-
-
 class TicketSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(TicketSerializer, self).validate(attrs=attrs)
+        Ticket.validate_ticket(
+            attrs["row"],
+            attrs["seat"],
+            attrs["flight"],
+            ValidationError,
+        )
+        return data
+
     class Meta:
         model = Ticket
-        fields = "__all__"
+        fields = ("id", "row", "seat", "flight")
 
 
 class FlightSerializer(serializers.ModelSerializer):
+    route = RouteListSerializer(read_only=True)
+    airplane = AirplaneSerializer(read_only=True)
+    crew = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="full_name"
+    )
+
+    class Meta:
+        model = Flight
+        fields = [
+            "id",
+            "departure_time",
+            "arrival_time",
+            "route",
+            "airplane",
+            "crew",
+        ]
+
+
+class FlightDetailSerializer(serializers.ModelSerializer):
     route = RouteDetailSerializer(read_only=True)
     airplane = AirplaneSerializer(read_only=True)
     crew = serializers.SlugRelatedField(
@@ -116,3 +141,42 @@ class FlightSerializer(serializers.ModelSerializer):
             "airplane",
             "crew",
         ]
+
+
+class TicketDetailSerializer(serializers.ModelSerializer):
+    flight = FlightSerializer(read_only=True)
+
+    class Meta:
+        model = Ticket
+        fields = "__all__"
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Order
+        fields = ("id", "tickets", "created_at", "user")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+
+            order = Order.objects.create(**validated_data)
+
+            for ticket_data in tickets_data:
+                Ticket.objects.create(
+                    order=order,
+                    **ticket_data,
+                )
+
+            return order
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    tickets = TicketDetailSerializer(many=True, read_only=False, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ("id", "tickets", "created_at")
