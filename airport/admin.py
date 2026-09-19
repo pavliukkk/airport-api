db2ad1ae23131aaf62f09724ada392fdb1bfc08f
Petro <petropavliuk12.05@gmail.com>
2026-09-19 19:22:50 +0300

    registered all models in admins.py and used inlines

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import (
+    Airport,
+    Airplane,
+    AirplaneType,
+    Order,
+    Ticket,
+    Crew,
+    Flight,
+    Route,
+)
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
+admin.site.register(Airport)
+admin.site.register(Airplane)
+admin.site.register(AirplaneType)
+admin.site.register(Ticket)
+admin.site.register(Crew)
+admin.site.register(Flight)
+admin.site.register(Route)
